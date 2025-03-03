1087128f22d6c8acb0c0311bea07434c28dfb40d
Mathias Jacobsen <mathias.jacobsen4@gmail.com>
2025-03-03 10:52:40 +0100

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Navigation } from './components/Navigation';
-import { HeroSection } from './components/HeroSection';
-import { BookingSection } from './components/BookingSection';
-import { ContactSection } from './components/ContactSection';
-import { ScrollToTop } from './components/ScrollToTop';
-import { Footer } from './components/Footer';
+import { 
+  Navigation, 
+  HeroSection, 
+  BookingSection, 
+  ContactSection, 
+  ScrollToTop, 
+  Footer 
+} from './components';
 
 export const App = () => (
   <main className="min-h-screen">
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { Navigation } from './Navigation';
+export { HeroSection } from './HeroSection';
+export { BookingSection } from './BookingSection';
+export { ContactSection } from './ContactSection';
+export { ScrollToTop } from './ScrollToTop';
+export { Footer } from './Footer';
