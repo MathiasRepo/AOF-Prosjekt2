011d74a00aa50ae08049a7ec0cfd82e4ef5571b7
Mathias Jacobsen <mathias.jacobsen4@gmail.com>
2025-03-03 10:59:13 +0100

    css edits

--- src/components/BookingSection.jsx
+++ src/components/BookingSection.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
 export const BookingSection = () => (
-  <section id="booking">
-    <h1 className="text-4xl font-bold text-center mb-8 mt-24">Book ditt kontorplass</h1>
-    <div className="w-full h-[820px] overflow-hidden my-12">
-      <div className="-mt-[16%] w-full h-[110%]">
-        <iframe
-          className="w-full h-full border-none overflow-hidden"
-          scrolling="no"
-          frameBorder="0"
-          src="https://www.supersaas.com/schedule/Slowboii/Phoenixres?partitioned=1"
-        />
-      </div>
+  <section id="booking" className="container mx-auto px-4 py-8">
+    <h1 className="text-4xl font-bold text-center mb-8">Book ditt kontorplass</h1>
+    <div className="iframe-container">
+      <iframe
+        scrolling="no"
+        frameBorder="0"
+        src="https://www.supersaas.com/schedule/Slowboii/Phoenixres?partitioned=1"
+      />
     </div>
   </section>
 );
