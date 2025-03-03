2677edf475ee18733aaae4c91e4a7de4ac126254
Thomas Andreassen <thoboii@gmail.com>
2025-03-03 11:06:39 +0100

    Delete index.js

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { Navigation } from './Navigation';
-export { HeroSection } from './HeroSection';
-export { BookingSection } from './BookingSection';
-export { ContactSection } from './ContactSection';
-export { ScrollToTop } from './ScrollToTop';
-export { Footer } from './Footer';
