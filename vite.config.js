b79fa6c223e4e192425f94716dbb3401798be8ca
Mathias Jacobsen <mathias.jacobsen4@gmail.com>
2025-03-10 10:16:08 +0100

    chore: build site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   base: '/AOF-Prosjekt2/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
